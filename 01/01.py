b66debbb509eb181b799530a1124051e843a9193
kstroz <kstroz24@gmail.com>
2020-12-11 15:31:56 +0100

    Changed function so its not repeating unnecessarily

--- 01/01.py
+++ 01/01.py
@@ -3,10 +3,12 @@ lines = [int(num) for num in file.readlines()]
 
 
 def find_sum(sum, numbers):
-    for i in numbers:
+    for index, i in enumerate(numbers):
         for j in numbers:
             if i + j == 2020:
                 return i * j
+
+        numbers.pop(index)
 
 
 print(find_sum(2020, lines))
