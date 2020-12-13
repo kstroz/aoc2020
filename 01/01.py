91b6e3c455ef9dd05e2cf4b449e936af9fdba7bf
kstroz <kstroz24@gmail.com>
2020-12-13 20:29:30 +0100

    Day 1 part 2 challenge solution

--- 01/01.py
+++ 01/01.py
@@ -2,13 +2,28 @@ file = open("input", "r")
 lines = [int(num) for num in file.readlines()]
 
 
-def find_sum(sum, numbers):
-    for index, i in enumerate(numbers):
-        for j in numbers:
-            if i + j == 2020:
-                return i * j
+def find_sum_from_two(sum, numbers):
+    numbers = numbers.copy()
+
+    for index, n1 in enumerate(numbers):
+        for n2 in numbers:
+            if n1 + n2 == 2020:
+                return n1 * n2
 
         numbers.pop(index)
 
 
-print(find_sum(2020, lines))
+def find_sum_from_three(sum, numbers):
+    numbers = numbers.copy()
+
+    for index, n1 in enumerate(numbers):
+        for n2 in numbers:
+            for n3 in numbers:
+                if n1 + n2 + n3 == 2020:
+                    return n1 * n2 * n3
+
+        numbers.pop(index)
+
+
+print(find_sum_from_two(2020, lines))
+print(find_sum_from_three(2020, lines))
