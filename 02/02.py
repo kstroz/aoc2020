dab0ffd1a66d648ba56eeb8ad21ad7344eb19383
kstroz <kstroz24@gmail.com>
2020-12-13 20:49:35 +0100

    Day 2 part 2 challenge solution

--- 02/02.py
+++ 02/02.py
@@ -2,11 +2,21 @@ file = open("input", "r")
 passwords = [password for password in file.readlines()]
 
 
-def password_validator(password):
+def old_password_validator(password):
     range_of, letter, password = password.split()
     n1, n2 = map(int, range_of.split("-"))
     range_of = range(n1, n2 + 1)
     return password.count(letter[0], 0, len(password)) in range_of
 
 
-print(len(list(filter(password_validator, passwords))))
+def new_password_validator(password):
+    range_of, letter, password = password.split()
+    letter = letter[0]
+    n1, n2 = map(int, range_of.split("-"))
+    answers = (password[n1 - 1] == letter, password[n2 - 1] == letter)
+
+    return len(set(answers)) == 2
+
+
+print(len(list(filter(old_password_validator, passwords))))
+print(len(list(filter(new_password_validator, passwords))))
